fcecb15d6b5c9b0fced1785f3e95015bacfa819d
James Aduke <jamesodwuor4@gmail.com>
2022-02-11 17:25:01 +0300

    card markup and styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,19 @@
+import Swimmer from "../images/katie-zaferes.png";
+import Star from "../images/star.png";
+
 export default function Card() {
     return (
-        <section>
-            <h3>Card</h3>
+        <section className="card">
+            <img src={Swimmer} alt="Swimmer" className="card--image"/>
+
+            <div className="card--stats">
+                <img src={Star} alt="Star" className="stats--image"/>
+                <span>5.0</span>
+                <span>(6)</span>
+                <span>USA</span>
+            </div>
+            <p className="card--description"> Life Lessons with Kate Zaferes</p>
+            <p className="card--description"><strong>From $136</strong>/ person</p>
 
         </section>
     )
