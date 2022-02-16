676409e903125b30edfc4a28f36666f98fa8235f
James Aduke <jamesodwuor4@gmail.com>
2022-02-16 09:11:14 +0300

    change component structure to use props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './style.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Swimmer from "./images/katie-zaferes.png";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={Swimmer}
+        rating={4}
+        reviewCount={10}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import Swimmer from "../images/katie-zaferes.png";
 import Star from "../images/star.png";
 
-export default function Card() {
+export default function Card({rating, reviewCount, country, title, price, img}) {
     return (
         <section className="card">
-            <img src={Swimmer} alt="Swimmer" className="card--image"/>
+            <img src={img} alt="Swimmer" className="card--image"/>
 
             <div className="card--stats">
                 <img src={Star} alt="Star" className="stats--image"/>
-                <span>5.0</span>
-                <span>(6)</span>
-                <span>USA</span>
+                <span>{rating}</span>
+                <span className="gray">{reviewCount}</span>
+                <span className="gray">{country}</span>
             </div>
-            <p className="card--description"> Life Lessons with Kate Zaferes</p>
-            <p className="card--description"><strong>From $136</strong>/ person</p>
+            <p className="card--description"> {title}</p>
+            <p className="card--description"><strong>From ${price}</strong>/ person</p>
 
         </section>
     )
