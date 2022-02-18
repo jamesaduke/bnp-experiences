edbbb4d5153fd181357290afdb09a850e46cec69
James Aduke <jamesodwuor4@gmail.com>
2022-02-18 12:26:02 +0300

    add some conditonal rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,15 @@ import Swimmer from "../images/katie-zaferes.png";
 import Star from "../images/star.png";
 
 export default function Card({key, title, description, price, coverImg, rating, reviewCount, location, openSpots}) {
+    let badgeText;
+    if (openSpots === 0) {
+        badgeText = "SOLD OUT";
+    }else if (location === "Online"){
+        badgeText = "ONLINE";
+    }
     return (
         <div className="card">
+            {badgeText && <div className="card--badge ">{badgeText}</div>}
             <img src={`/images/${coverImg}`} alt="Swimmer" className="card--image"/>
 
             <div className="card--stats">
